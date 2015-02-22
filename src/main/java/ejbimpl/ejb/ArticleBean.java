f5e55fc674ac199ad9c6a13020f46a67c983b905
Damien Carton <carton.damien@gmail.com>
2015-02-22 18:15:08 +0100

    c'est bon t'as ta fonction

--- src/main/java/ejbimpl/ejb/ArticleBean.java
+++ src/main/java/ejbimpl/ejb/ArticleBean.java
@@ -2,18 +2,16 @@ package ejbimpl.ejb;
 
 import java.util.List;
 
-import ejbimpl.ejb.*;
+import javax.ejb.Stateless;
+
 import ejbinterface.factory.ModelFactory;
 import ejbinterface.interfaces.ArticleLocal;
 import ejbinterface.interfaces.ArticleRemote;
 import ejbinterface.model.ArticleShared;
-import ejbinterface.model.UserShared;
 import ejbpersistance.dao.ArticleDao;
 import ejbpersistance.dao.UserDao;
 import ejbpersistance.entities.Article;
 import ejbpersistance.entities.User;
-
-import javax.ejb.Stateless;
 
 @Stateless
 public class ArticleBean implements ArticleLocal, ArticleRemote {
@@ -46,22 +44,36 @@ public class ArticleBean implements ArticleLocal, ArticleRemote {
 	}
 
 	public ArticleShared findOne(Object id) {
-		// TODO Auto-generated method stub
-		return null;
+		ArticleDao adao = new ArticleDao();
+		
+		Article a = new Article();
+		a = adao.get((Integer)id);
+		try {
+			return ModelFactory.convert(ArticleShared.class, a);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}	
 	}
 
-	public ArticleShared save(String title, String content, UserShared user) {
+
+	public ArticleShared save(String title, String content, Object userID) {
 		ArticleDao adao = new ArticleDao();
+		UserDao udao = new UserDao();
 		
 		Article a = new Article();
 		a.setTitle(title);
 		a.setContent(content);
-		User u = (User)ModelFactory.convert(UserShared.class, user);
+		User u = udao.get((Integer)userID);
 		a.setUser(u);
 		
-		adao.save(a);
-		
-		return true;
+		Article az = adao.save(a);
+		try {
+			return ModelFactory.convert(ArticleShared.class, az);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}		
 	}
 
 }
